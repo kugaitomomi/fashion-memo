62e6ab8c44733da241f07937f58d5b448ff2752b
kugaitomomi <tottomo7@gmail.com>
2022-08-22 11:38:17 +0900

    feat: make submit photo and text

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,28 @@ import { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
-
+  const [pic, setPic] = useState();
   const [allPics, setAllPics] = useState([]);
+  const [ category, setCategory ] = useState();
+  const [ note, setNote ] = useState();
   useEffect(() => {
     getAllInfo();
   },[allPics]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    console.log(pic);
+    formData.append("pic", pic);
+    formData.append("category", category);
+    formData.append("note", note);
+    await axios.post("http://localhost:5000/addPicture", formData).then(getAllInfo()).catch((error) => console.log(error));
+  }
+
+    const handleChange = (e) => {
+    setPic(e.target.files[0]);
+    console.log(e.target.files[0]);
+  }
 
   const getAllInfo = async () => {
     await axios.get('http://localhost:5000/pictures').then(res => {
@@ -28,15 +45,32 @@ function App() {
       .catch((error) => console.log(error.message));
   };
 
+  const selectChange = (e) => {
+    setCategory(e.target.value);
+  }
 
+  const insertChange = (e) => {
+    setNote(e.target.value);
+  }
 
   return (
     <div className="App">
-
+    <form onSubmit={handleSubmit}>
+      <input type="file" className="input" onChange={handleChange} />
+      <select name="example" value={category} onChange={selectChange}>
+        <option value="Daily">Daily</option>
+        <option value="Special">Special</option>
+        <option value="Date">Date</option>
+        <option value="Home">Home</option>
+        <option value="メルカリ">メルカリ</option>
+        <option value="ラクマ">ラクマ</option>
+      </select>
+      <input type="text" value={note} onChange={insertChange} />
+      <button className="submit" >upload</button>
+    </form>
     <div className="imgsContainer">
       {allPics && allPics.map((pic, index) => {
         return <div className="imgItem" key={index}><img src={pic.url} alt={pic.name} width="50%" /><p>{pic.category}</p><p>{pic.note}</p><button className="imgButton" onClick={() => handleDelete(pic.name)}>Delete</button></div>
-      
       })}
       
     </div>
