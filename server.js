d6da82a3343fce8532703d11e7836e0b4e8baec8
kugaitomomi <tottomo7@gmail.com>
2022-08-22 11:19:16 +0900

    feat: make getting all picture

--- server.js
+++ server.js
@@ -50,3 +50,38 @@ app.post("/addpicture", upload.single("pic"),async (req, res) => {
 });
 
 
+//get all picture
+app.get("/pictures", async(req, res) => {
+  //↓↓fire storageに格納されているファイルを定数に格納している。
+  const listRef = ref(storage);
+
+  //↓↓配列にして、for文で一つ一つ取り出してデータをJson形式で表示している。
+  const productPictures = [];
+  await listAll(listRef).then(async (pics) => {
+  for(const item of pics.items){
+    //firebaseに格納されているファイルのurl取得
+      const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${item._location.bucket}/o/${item._location.path_}?alt=media`;
+      //knexに格納されているデータの取得
+      //knex.select(取得したい情報の名前1, 取得したい情報の名前2・・・).from(テーブル名).where(カラム名, 照らし合わせたい情報名(今回は、ヘッダー情報の(item.name)).first();
+      //↓今回は画像名をキーにして、呼び出したい画像名とDBに格納されている画像名を照らし合わせて取得している。
+      const serachResult = await knex.select("category", "note").from("fashion").where("name", item.name).first();
+      //「定数・変数名?.プロティ」の「?」は、もし該当する値があれば。という意味。
+      const category = serachResult?.category;
+      const note = serachResult?.note;
+      console.log(item.name);
+      console.log(serachResult);
+      //配列に情報を格納している↓
+      productPictures.push({
+        url: publicUrl,
+        name: item._location.path_,
+        category,
+        note,
+      });
+  };
+  //配列にして、情報をクライアントに投げている。↓
+    res.status(200).send(productPictures);
+  })
+  .catch((err) => console.error(err));
+})
+
+
